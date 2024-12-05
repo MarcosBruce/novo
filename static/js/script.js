81c8901f814894c3862be775863de24b43929177
MarcosBruce <marcosbrucex@gmail.com>
2024-12-04 21:49:28 -0400

    V1.5

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+function validarSenhaCadastro() {
+    const senha = document.getElementById("senha").value;
+    const confirmarSenha = document.getElementById("confirmar_senha").value;
+    if (senha !== confirmarSenha) {
+        alert("As senhas não coincidem!");
+        return false;
+    }
+    return true;
+}
